07823979feaf432f3ba0622ec597aae6a8a55f9b
Hao Guo <49539832+guohao1995@users.noreply.github.com>
2024-04-02 16:56:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World! Github"
+
+@app.route('/api/headers')
+def headers():
+    json_res = jsonify({ "message": "Adding custom header.." })
+    res = make_response(json_res)
+    res.headers['X-Frame-Options'] = 'DENY'
+    return res
