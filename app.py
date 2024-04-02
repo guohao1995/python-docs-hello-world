6da852edbfc802afc1f609b3dbcd7ff284b7a13e
Hao Guo <49539832+guohao1995@users.noreply.github.com>
2024-04-02 17:51:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def headers():
     json_res = jsonify({ "message": "Adding custom header.." })
     res = make_response(json_res)
     res.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    res.headers['Content-Security-Policy'] = 'frame-ancestors 'self' https://hao-python-test.azurewebsites.net/api/headers;'
     return res
